63d968144d9de6f9ca7805ed4b9fec800ff1eab6
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-08-05 16:54:09 +0800

    using the appstate pointer instead of static vars

--- tetri_cpp/src/tetri_cpp.cpp
+++ tetri_cpp/src/tetri_cpp.cpp
@@ -13,21 +13,29 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window* window = NULL;
-static SDL_Renderer* renderer = NULL;
+struct AppState
+{
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+};
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
-    static_cast<void>(appstate);
     static_cast<void>(argc);
     static_cast<void>(argv);
 
+    static AppState state{};
+
     /* Create the window */
-    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &state.window, &state.renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
+    // store the window and renderer in appstate to be passed around in other SDL_App??? functions
+    *appstate = &state;
+
     return SDL_APP_CONTINUE;
 }
 
@@ -46,7 +54,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    static_cast<void>(appstate);
+    AppState* state = static_cast<AppState*>(appstate);
 
     const char* message = "Hello World!";
     int w = 0, h = 0;
@@ -54,17 +62,17 @@ SDL_AppResult SDL_AppIterate(void* appstate)
     const float scale = 4.0f;
 
     /* Center the message and scale it up */
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
+    SDL_GetRenderOutputSize(state->renderer, &w, &h);
+    SDL_SetRenderScale(state->renderer, scale, scale);
     x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
     y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
     /* Draw the message */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
+    SDL_RenderClear(state->renderer);
+    SDL_SetRenderDrawColor(state->renderer, 255, 255, 255, 255);
+    SDL_RenderDebugText(state->renderer, x, y, message);
+    SDL_RenderPresent(state->renderer);
 
     return SDL_APP_CONTINUE;
 }
