e517ca1ae04969673e68fed81ba5d087d16f0fd8
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-08-06 18:36:30 +0800

    basic drawing!

--- tetri_cpp/src/Board.h
+++ tetri_cpp/src/Board.h
@@ -8,9 +8,9 @@
 
 // -- Defines --
 
-constexpr int BOARD_LINE_WIDTH = 6;       // Width of each of the two lines that delimit the board
-constexpr int BLOCK_SIZE = 16;            // Width and Height of each block of a piece
-constexpr int BOARD_POSITION = 320;       // Center position of the board from the left of the screen
+constexpr int BOARD_LINE_WIDTH = 8;       // Width of each of the two lines that delimit the board
+constexpr int BLOCK_SIZE = 32;            // Width and Height of each block of a piece
+constexpr int BOARD_POSITION = 1920 / 2;  // Center position of the board from the left of the screen
 constexpr int BOARD_WIDTH = 10;           // Board width in blocks
 constexpr int BOARD_HEIGHT = 20;          // Board height in blocks
 constexpr int MIN_VERTICAL_MARGIN = 20;   // Minimum vertical margin for the board limit
