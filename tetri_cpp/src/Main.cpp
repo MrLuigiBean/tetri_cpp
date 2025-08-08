3126f0fd08f066e8c8cfc4f6b09927d91526b6bd
MrLuigiBean <84760999+MrLuigiBean@users.noreply.github.com>
2025-08-08 16:43:55 +0800

    Update Main.cpp

--- tetri_cpp/src/Main.cpp
+++ tetri_cpp/src/Main.cpp
@@ -39,8 +39,6 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-    static_cast<void>(appstate);
-
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
